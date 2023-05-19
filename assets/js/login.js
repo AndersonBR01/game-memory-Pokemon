0d2b5aea56a53cab3bc34be3bd3d4ee60a7d2d7e
Anderson <andersoncsilva850@gmail.com>
2023-05-19 11:38:22 -0300

    ideia

---
+++ assets/js/login.js
@@ -0,0 +1,27 @@
+const input = document.querySelector('.login__input');
+const button = document.querySelector('.login__button');
+const form =  document.querySelector('.login__form');
+
+
+const validateInput =({target}) =>{
+    if (target.value.length > 2){
+        button.removeAttribute('disabled', );
+        return
+    }
+    button.setAttribute('disabled', '')
+    
+
+}
+ const handleSubimt = (event) =>{
+    event.preventDefault();
+
+     localStorage.setItem('player', input.value)
+     window.location= '/game.html'
+
+     
+ }
+
+
+input.addEventListener('input', validateInput);
+
+form.addEventListener('submit', handleSubimt);
